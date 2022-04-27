6a1a3dc1e6a138a4a7e6cdad868f62724a19479b
Akshitha Reddy <akshithareddy1899@gmail.com>
2022-04-27 14:43:23 +0530

    Fix linters

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   root 'users#index'
 
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show]
   end
 end
