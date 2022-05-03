81255df61ff5b7a7f3a13b787f36ba324e773206
Akshitha Reddy <akshithareddy1899@gmail.com>
2022-05-03 11:48:05 +0530

    Fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
-class User < ApplicationRecord
-	has_many :posts
-	has_many :likes
-	has_many :comments
+# frozen_string_literal: true
 
-	def return_three_most_recent_posts
-		Post.order('created_at DESC').limit(3)
-	end
+class User < ApplicationRecord
+  has_many :posts
+  has_many :likes
+  has_many :comments
+
+  def return_three_most_recent_posts
+    Post.order('created_at DESC').limit(3)
+  end
 end
--- db/migrate/20220503030229_create_posts.rb
+++ db/migrate/20220503030229_create_posts.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
@@ -5,7 +7,7 @@ class CreatePosts < ActiveRecord::Migration[7.0]
       t.string :Text
       t.integer :CommentsCounter
       t.integer :LikesCounter
-      t.references :author, null: false, foreign_key: {to_table: :users}
+      t.references :author, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
     end
