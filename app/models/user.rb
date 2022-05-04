027e20ef2f08db7097f7cca2dd9fc64bb4ec2c67
Akshitha Reddy <akshithareddy1899@gmail.com>
2022-05-04 23:17:34 +0530

    Implement requested changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
 
-  has_many :likes
-  has_many :comments
+  has_many :likes, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id'
 
   def return_three_most_recent_posts
     Post.order('created_at DESC').where(author_id: id).limit(3)
