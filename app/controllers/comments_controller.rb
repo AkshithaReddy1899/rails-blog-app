4b07f84cd8f578963f9929aec0e845db2414dc50
Akshitha Reddy <akshithareddy1899@gmail.com>
2022-05-05 21:08:20 +0530

    Fix linter errors

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,10 +5,8 @@ class CommentsController < ApplicationController
 
   def create
     new_comment = Comment.new(post_params)
-      if new_comment.save
-        redirect_to user_post_path(new_comment.author_id, new_comment.post_id)
-      end
-      redirect_to "/users/#{@new_comment.author.id}.posts/#{new_comment.post_id}", notice: 'Success!'
+    redirect_to user_post_path(new_comment.author_id, new_comment.post_id) if new_comment.save
+    redirect_to "/users/#{@new_comment.author.id}.posts/#{new_comment.post_id}", notice: 'Success!'
   end
 
   def post_params
