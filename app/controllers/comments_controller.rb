688eccd8f1cf93b3f23e6136a6c748c134f9ccdf
Akshitha Reddy <akshithareddy1899@gmail.com>
2022-05-05 19:15:25 +0530

    Revert "Create comments"
    This reverts commit ac0128b864c38c9fca64588a409519758f5d9912.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,22 +1,4 @@
 class CommentsController < ApplicationController
   def new
-    @comment = Comment.new
-    @user = current_user
-    @post = Post.find(params[:post_id])
-  end
-  def create
-    @new_comment = current_user.comments.new(comment_params)
-    respond_to do |format|
-      format.html do
-        if @new_comment.save
-          redirect_to "/users/#{@new_comment.author.id}.posts/", notice: 'Success!'
-        else
-          render :new, alert: 'Error! Post not saved try again.'
-        end
-      end
-    end
-  end
-  def comment_params
-    params.require(:comment).permit(:Text)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show new create] do
-      resources :comments, only: %i[new, create]
+      resources :comments, only: %i[:new]
       resources :likes, only: %i[:new]
     end
   end
