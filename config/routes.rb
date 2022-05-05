cc0a69c987a32c18ef05c9faf56a58c306ef38b8
Akshitha Reddy <akshithareddy1899@gmail.com>
2022-05-05 10:48:50 +0530

    Create comments, likes controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'likes/new'
+  get 'comments/new'
   root 'users#index'
 
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
+    resources :posts, only: %i[index show new] do
+      resources :comments, only: %i[:new]
+      resources :likes, only: %i[:new]
+    end
   end
 end
