79b25e39f519b2b751781b28282a68c02c0f9a66
Akshitha Reddy <akshithareddy1899@gmail.com>
2022-05-06 21:20:55 +0530

    Method test for user and post

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,25 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  subject { Post.new(Title: 'Hello', Text: 'Hello World!',CommentsCounter:2 , LikesCounter: 0, author_id: 3 )}
-  
-  before { subject.save }
 
+  before do(:all)
+    @post = Post.new(Title: 'Hello', Text: 'Hello World!',CommentsCounter:2 , LikesCounter: 0, author_id: 3 )
+  end
+  
   it 'Title should be present' do
-    subject.Title = nil
-    expect(subject).to_not be_valid
+    @post.Title = nil
+    expect(@post).to_not be_valid
   end
 
   it "Text should be present" do
-    subject.Text = nil
-    expect(subject).to_not be_valid
+    @post.Text = nil
+    expect(@post).to_not be_valid
   end
 
   it "CommentsCounter should be greater than or equal to 0" do
-    expect(subject.CommentsCounter).to be >=0
+    expect(@post.CommentsCounter).to be >=0
   end
 
   it "LikesCouter should be greater than or equal to 0" do
-    expect(subject.LikesCounter).to be >=0
+    expect(@post.LikesCounter).to be >=0
+  end
+
+  it 'Should return five most recent comments' do
+    length = @post.return_five_most_recent_comments.length
+    expect(length).to be <= 5
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,18 +2,23 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  subject { User.new(Name: "Tom", Photo: 'user.png', Bio: 'Teacher from Mexico', PostsCounter: 1) }
-
-  before { subject.save }
+  before(:all) do
+    @user = User.new(Name: "Tom", Photo: 'user.png', Bio: 'Teacher from Mexico', PostsCounter: 1)
+  end
 
   context 'User validation test' do
     it 'ensures Name presence' do
-      subject.Name = ''
-      expect(subject).to_not be_valid
+      @user.Name = ''
+      expect(@user).to_not be_valid
     end
 
     it 'ensures PostsCounter greater than equal to 0' do
-      expect(subject.PostsCounter).to be >= 0
+      expect(@user.PostsCounter).to be >= 0
     end
+  end
+
+  it 'User returns three most recent posts' do
+    length = @user.return_three_most_recent_posts.length
+    expect(length).to be <= 3
   end
 end
