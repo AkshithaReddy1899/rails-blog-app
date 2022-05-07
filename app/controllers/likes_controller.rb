06a7746a752749d275ac1c4ca8e39bfef9f27e55
Akshitha Reddy <akshithareddy1899@gmail.com>
2022-05-07 11:09:03 +0530

    flash

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,7 +3,13 @@ class LikesController < ApplicationController
     @post = Post.find(params[:post_id])
     new_like = current_user.likes.new
     new_like.post_id = @post.id
-    new_like.save
-    redirect_to user_post_path(@post.author_id, @post.id)
+
+    if new_like.save
+      flash[:notice] = 'Like added'
+      redirect_to user_post_path(@post.author_id, @post.id)
+    else
+      flash[:alert] = 'Like not added. Try again'
+      redirect_to user_post_path(@post.author_id, @post.id)
+    end
   end
 end
