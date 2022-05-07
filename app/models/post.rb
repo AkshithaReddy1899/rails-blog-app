8a9bf302c5a64eec1206bda7a94005bf6ea20722
Akshitha Reddy <akshithareddy1899@gmail.com>
2022-05-07 12:14:22 +0530

    Add validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,11 @@ class Post < ApplicationRecord
 
   has_many :likes
   has_many :comments
+  
+  validates :Title, presence: true, length: { maximum: 250 }
+  validates :CommentsCounter, numericality: { greater_than_or_equal_to: 0}
+  validates :LikesCounter, numericality: { greater_than_or_equal_to: 0}
+
   after_save :update_posts_counter
 
   def update_posts_counter
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
 
+  validates :Name, presence: true
+  validates :PostsCounter, numericality: { greater_than_or_equal_to: 0 }
+
   def return_three_most_recent_posts
     Post.order('created_at DESC').where(author_id: id).limit(3)
   end
