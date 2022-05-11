c12e13e7906a754d517dd88bceb32232056c2f40
Akshitha Reddy <akshithareddy1899@gmail.com>
2022-05-11 15:02:36 +0530

    Reset password

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,14 @@ Rails.application.configure do
   # For letter_opener
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password:ENV["GMAIL_PASSWORD"]
+  }
 end
