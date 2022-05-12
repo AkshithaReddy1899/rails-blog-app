15ad76cb7cec419cc28760d3bd1a8d5bb5a2ef98
Akshitha Reddy <akshithareddy1899@gmail.com>
2022-05-12 15:10:47 +0530

    Delete comment functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,4 +18,21 @@ class CommentsController < ApplicationController
       redirect_to "/users/#{current_user.id}/posts}"
     end
   end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    user = User.find(@post.author_id)
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    if @comment.destroy
+      @post.CommentsCounter -= 1
+      user.save
+      flash[:notice] = "You have deleted this comment successfully!"
+      redirect_to user_posts_path(user.id)
+    else
+      flash[:alert] = 'Comment not deleted. Try again'
+      redirect_to user_post_path(@post.author_id)
+    end
+  end
+
 end
